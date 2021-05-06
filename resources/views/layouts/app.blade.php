a0bbd806cb3512dcce85680d8934c24adb8f7ab6
Sebastiano <pasca.sebastiano5@libero.it>
2021-05-06 18:07:39 +0200

    Try

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Transmolisana</title>
+</head>
+<body>
+    <div>ciao</div>
+</body>
+</html>
